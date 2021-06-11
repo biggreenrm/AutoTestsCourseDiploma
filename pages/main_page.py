e1630dde9f6bdf2b9795c56ad9ff8c8fb93f14ea
biggreenrm <biggreen.rm@gmail.com>
2021-06-11 17:57:50 +0300

    [IMP/REF] main: move method to base class / develop test for login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,23 +1,16 @@
 #firstparty
 from .base_page import BasePage
+from .locators import MainPageLocators
 
 #thirdparty
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 
 
 class MainPage(BasePage):
     
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
-    
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click() 
     
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link")
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK)
