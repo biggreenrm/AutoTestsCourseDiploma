54a0e59d14eab0f2cf0b2851a5669fcce16de690
biggreenrm <biggreen.rm@gmail.com>
2021-06-14 12:38:23 +0300

    [IMP] main: make test_product_page parametrizable

--- test_product_page.py
+++ test_product_page.py
@@ -1,11 +1,17 @@
 #firstparty
 from .pages.product_page import ProductPage
+from .constants import SET_OF_PRODUCT_LINKS
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#thirdparty
+import pytest
 
 
-def test_guest_can_add_product_to_basket(browser):
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
+
+@pytest.mark.parametrize("link", SET_OF_PRODUCT_LINKS)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
