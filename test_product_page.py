a84d35a1da02585a6a4c4a7b3d6af1e1e065cfb5
biggreenrm <biggreen.rm@gmail.com>
2021-06-16 18:19:25 +0300

    [REF] main: reset marks to proper tests

--- test_product_page.py
+++ test_product_page.py
@@ -16,7 +16,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
 
-@pytest.mark.need_review
+
 class TestUserAddToBasketFromProductPage:
     
     @pytest.fixture(scope="function", autouse=True)
@@ -33,7 +33,8 @@ class TestUserAddToBasketFromProductPage:
         page = ProductPage(browser, link)
         page.open()
         page.should_not_be_present_success_message()
-
+        
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, link)
         page.open()
@@ -47,6 +48,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_present_success_message()
 
+@pytest.mark.need_review
 @pytest.mark.parametrize("link", SET_OF_PRODUCT_LINKS)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
@@ -75,6 +77,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
@@ -82,6 +85,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
